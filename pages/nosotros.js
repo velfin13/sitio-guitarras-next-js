763c7b305c6cb4ca46c3ff2cce34b76e983c3f07
Velfin Velasquez <vvelasquez2465@utm.edu.ec>
2022-05-22 22:57:09 -0500

    estilos header css

---
+++ pages/nosotros.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+const Nosotros = () => {
+  return (
+    <Layout titlePage="Nosotros">
+      <h1>Desde nosotros</h1>
+    </Layout>
+  );
+};
+
+export default Nosotros;
