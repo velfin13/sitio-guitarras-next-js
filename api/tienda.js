3daee795dc419200eee951dcbcbe1306023ef5f3
Velfin Velasquez <vvelasquez2465@utm.edu.ec>
2022-05-25 09:38:16 -0500

    routing dinamico

--- api/tienda.js
+++ api/tienda.js
@@ -1,12 +1,23 @@
 export const getGuitarras = async () => {
-    try {
-      const url = `${process.env.NEXT_PUBLIC_BASE_URL_API}/guitarras`;
-      const respuesta = await fetch(url);
-  
-      const resultado = await respuesta.json();
-      return resultado;
-    } catch (error) {
-      return error;
-    }
-  };
-  
+  try {
+    const url = `${process.env.NEXT_PUBLIC_BASE_URL_API}/guitarras`;
+    const respuesta = await fetch(url);
+
+    const resultado = await respuesta.json();
+    return resultado;
+  } catch (error) {
+    return error;
+  }
+};
+
+export const getGuitarraByURL = async (url) => {
+  try {
+    const urlApi = `${process.env.NEXT_PUBLIC_BASE_URL_API}/guitarras?url=${url}`;
+    const respuesta = await fetch(urlApi);
+
+    const resultado = await respuesta.json();
+    return resultado;
+  } catch (error) {
+    return error;
+  }
+};
--- pages/guitarras/[url].js
+++ pages/guitarras/[url].js
@@ -1,12 +1,43 @@
-import React from "react";
+import Image from "next/image";
+import { formatCantidadToMoneda } from "../../helpers";
 import Layout from "../../components/Layout";
+import { getGuitarraByURL } from "../../api/tienda";
 
-const GuitarrasPage = () => {
+import styles from "../../styles/Guitarra.module.css";
+
+const GuitarrasPage = ({ guitarra = [] }) => {
+  const { description, price, name, imagen } = guitarra[0];
+  console.log(description);
+
   return (
-    <Layout>
-      <div>GuitarrasPage</div>
+    <Layout titlePage={name}>
+      <div className={styles.guitarra}>
+        <Image
+          layout="responsive"
+          width={180}
+          height={350}
+          src={imagen[0].url}
+          priority="true"
+          alt="img"
+        />
+        <div className={styles.contenido}>
+          <h3>{name}</h3>
+          <p className={styles.description}>{description}</p>
+          <p className={styles.precio}>{formatCantidadToMoneda(price)}</p>
+          {/* <Link href={`/guitarras/${url}`}>
+          <a className={styles.enlace}>Ver más ...</a>
+        </Link> */}
+        </div>
+      </div>
     </Layout>
   );
 };
+
+export async function getServerSideProps({ query: { url } }) {
+  const guitarra = await getGuitarraByURL(url);
+  return {
+    props: { guitarra },
+  };
+}
 
 export default GuitarrasPage;
